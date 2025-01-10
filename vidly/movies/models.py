c2e169f3c4286cd37d4f31fe9701ba0e450b2059
thee-falcon <omar.makran.10@gmail.com>
2025-01-10 23:04:29 +0100

    create models

--- vidly/movies/models.py
+++ vidly/movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Gener(models.Model):
+    name = models.CharField(max_length=255)
+
+class  Movies(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    gener = models.ForeignKey(Gener, on_delete=models.CASCADE)
+
