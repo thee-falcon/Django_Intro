db323ca8aed22fc929d272b81bb66f697b63b3d9
thee-falcon <omar.makran.10@gmail.com>
2025-01-12 23:20:27 +0100

    database abstraction API

--- vidly/movies/views.py
+++ vidly/movies/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movies
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    # SELECT  FROM movies_movie
+    movies = Movies.objects.all()
+    output = ', '.join([movie.title for movie in movies])
+    return HttpResponse(output)
